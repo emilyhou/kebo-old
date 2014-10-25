1a03f76cb108798dd85ae068df7c954fc22a8ecc
unknown <gt224@cornell.edu>
2014-10-25 16:44:56 -0400

    Add swipe left and right

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,30 @@
 angular.module('demo.controllers', [])
 
-.controller('DishCtrl', function($scope) {
+.controller('MainCtrl', function($scope, Queue) {
+    $scope.dishes = Queue.all();
+    $scope.dish = $scope.dishes[0];
     
+    $scope.onSwipeRight = function(){
+        var index = $scope.dishes.indexOf(this.dish);
+        var nextItem;
+        if(index >= 0 && index < this.dishes.length - 1){
+            nextItem = this.dishes[index + 1]
+        } else {
+            nextItem = this.dishes[0];
+        }
+        this.dish = nextItem;
+    }
+    
+    $scope.onSwipeLeft = function(){
+        var index = $scope.dishes.indexOf(this.dish);
+        var nextItem;
+        if(index > 0 && index <= this.dishes.length - 1){
+            nextItem = this.dishes[index - 1]
+        } else {
+            nextItem = this.dishes[this.dishes.length-1];
+        }
+        this.dish = nextItem;
+    }
 })
 
 .controller('QueueCtrl', function($scope) {
